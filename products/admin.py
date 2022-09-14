cc5d3dbfb3a39a60638e912413068153be95fc0b
marcussteel <resetyourcoin21@gmail.com>
2022-09-14 20:23:20 +0300

    first commit

---
+++ products/admin.py
@@ -0,0 +1,12 @@
+
+# Register your models here.
+from django.contrib import admin
+from .models import Product
+
+admin.site.register(Product)
+
+admin.site.site_title = "Clarusway Title"
+admin.site.site_header = "Clarusway Admin Portal"
+admin.site.index_title = "Welcome to Clarusway Admin Portal"
+
+
